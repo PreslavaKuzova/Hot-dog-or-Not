873a5c96cb9ca1a8ff4639641d25e1cfee742498
PreslavaKuzova <preslava.kuzova@gmail.com>
2020-01-29 15:49:51 +0800

    Initial model design

---
+++ src/model/Food.java
@@ -0,0 +1,48 @@
+package model;
+
+import com.google.gson.Gson;
+
+public class Food {
+    private String fdcId;
+    private String description;
+    private String gtinUpc;
+    private String[] ingredients;
+    private Nutrients nutrients;
+
+    public Food(String fdcId, String description, String gtinUpc, String[] ingredients) {
+        this.fdcId = fdcId;
+        this.description = description;
+        this.gtinUpc = gtinUpc;
+        this.ingredients = ingredients;
+    }
+
+    public Food createFoodFromJson(String json) {
+        Food food = new Gson().fromJson(json, Food.class);
+        //make a request and maybe set it to the other element as well
+        return food;
+    }
+
+    public String getFdcId() {
+        return fdcId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getGtinUpc() {
+        return gtinUpc;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public Nutrients getNutrients() {
+        return nutrients;
+    }
+
+    public void setNutrients(Nutrients nutrients) {
+        this.nutrients = nutrients;
+    }
+}
