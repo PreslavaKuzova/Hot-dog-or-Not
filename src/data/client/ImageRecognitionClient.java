7235f93f6b549be540f49f5dcaaf466c846feff9
PreslavaKuzova <preslava.kuzova@gmail.com>
2020-02-08 22:07:21 +0800

    Gradle support, more functionality added

--- src/data/client/ImageRecognitionClient.java
+++
@@ -1,5 +0,0 @@
-package data.client;
-
-public class ImageRecognitionClient {
-    public ImageRecognitionClient() { }
-}
---
+++ src/main/java/utils/io/devices/FileDevice.java
@@ -0,0 +1,13 @@
+package utils.io.devices;
+
+public class FileDevice implements IODevice {
+    @Override
+    public String read() {
+        return null;
+    }
+
+    @Override
+    public void write(String data) {
+
+    }
+}
